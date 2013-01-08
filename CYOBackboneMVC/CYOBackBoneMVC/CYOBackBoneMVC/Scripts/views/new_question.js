2729a4b2c0d138b95b25115fa5dc1417c2a77501
Jason Robey <jason.robey@gmail.com>
2013-01-08 15:25:20 -0500

    Fixed view bug and add name

---
+++ CYOBackboneMVC/CYOBackBoneMVC/CYOBackBoneMVC/Scripts/views/new_question.js
@@ -0,0 +1,14 @@
+﻿App.Views.NewQuestion = Backbone.View.extend({
+
+    initialize: function (options) {
+        App.currentUser.on("change", this.render, this);
+        this.render();
+    },
+
+    render: function () {
+        this.$el.toggleClass("hidden", !App.currentUser.isLoggedIn());
+        this.$("#asker").text(App.currentUser.get("userName"));
+        return this;
+    }
+
+});
