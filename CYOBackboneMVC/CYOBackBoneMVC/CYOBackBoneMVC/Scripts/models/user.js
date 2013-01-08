984dfb0b23bcc16b40e5e4faf46ce32cc8e7affd
Jason Robey <jason.robey@gmail.com>
2013-01-08 14:34:10 -0500

    create user model

---
+++ CYOBackboneMVC/CYOBackBoneMVC/CYOBackBoneMVC/Scripts/models/user.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../jquery-1.7.2.intellisense.js" />
+/// <reference path="../backbone.js" />
+
+App.Models.User = Backbone.Model.extend({
+    logIn: function (userName) {
+        if (!userName) return;
+        this.set("userName", userName);
+    },
+    
+    isLoggedIn: function () {
+        return this.has('userName');
+    },
+});
